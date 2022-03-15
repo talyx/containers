304ab7b1d0c5bf1d45b0ebbe1309839e64d85c8e
Talyx <farruh.rahmatov40@gmail.com>
2022-03-16 01:11:57 +0300

    misk

--- iterators/iterator.hpp
+++ iterators/iterator.hpp
@@ -1,19 +1,25 @@
-#ifndef ITERATOR_HPP
-#define ITERATOR_HPP
+#ifndef ITERATORS_ITERATOR_HPP_
+#define ITERATORS_ITERATOR_HPP_
 
+#include <stddef.h>
 #include <iostream>
 
-template <typename Category, typename T, typename Pointer = T*, typename Reference = T&>
+//========================================================
+// main struct
+//========================================================
+
+template <typename Category, typename T, typename Distance = ptrdiff_t, typename Pointer = T*, typename Reference = T&>
 struct iterator {
 	public:
 		typedef Category		iterator_category;
 		typedef T				value_type;
+		typedef Distance		difference_type;
 		typedef Pointer			pointer;
 		typedef Reference		reference;
 };
 
 //=========================================================
-//Iterator category
+// Iterator category
 //=========================================================
 
 struct input_iterator_tag {};
@@ -22,34 +28,53 @@ struct bidirectional_iterator_tag: public forward_iterator_tag {};
 struct random_acces_iterator_tag: public bidirectional_iterator_tag {};
 
 //========================================================
-//iterator_traits
+// iterator_traits
 //========================================================
 
-
-template <class Iterator> 
-	struct iterator_traits{
-		public:
-			typedef typename Iterator::difference_type	difference_type;
-			typedef typename Iterator::value_type		value_type;
-			typedef typename Iterator::pointer			pointer;
-			typedef typename Iterator::reference			reference;
-			typedef typename Iterator::iterator_category iterator_category;
-	};
+template <class Iterator>
+struct	iterator_traits {
+ public:
+	typedef typename Iterator::difference_type	difference_type;
+	typedef typename Iterator::value_type		value_type;
+	typedef typename Iterator::pointer			pointer;
+	typedef typename Iterator::reference			reference;
+	typedef typename Iterator::iterator_category iterator_category;
+};
 
 template <class T>
-	class iterator_traits<T* >{
-		public:
-			ptrdiff_t					difference_type;
-			T							value_type;
-			T*							pointer;
-			T&							reference;
-			random_acces_iterator_tag	iterator_category;
-	};
+class iterator_traits<T* > {
+ public:
+	ptrdiff_t					difference_type;
+	T							value_type;
+	T*							pointer;
+	T&							reference;
+	random_acces_iterator_tag	iterator_category;
+};
 
-template <class Iterator>
-	class iterator_traits<const Iterator*>{
-		public:
-			ptrdiff_t					difference_type;
-			T							value_type;
-	};
-#endif
+template <class T>
+class iterator_traits<const T*> {
+ public:
+	ptrdiff_t					difference_type;
+	T							value_type;
+	const T*					pointer;
+	const T&					reference;
+	random_acces_iterator_tag	iterator_category;
+};
+
+//========================================================
+// conditional
+//========================================================
+
+template <bool, typename A, typename B>
+struct conditional {
+	typedef B type;
+};
+
+template <typename A, typename B>
+struct conditional<true, A, B> {
+	typedef A type;
+};
+
+
+#endif // ITERATORS_ITERATOR_HPP_
+
