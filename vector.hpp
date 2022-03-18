e993d948ac8469128f49162e760b5d79122861f1
Talyx <farruh.rahmatov40@gmail.com>
2022-03-18 21:48:31 +0300

    misc

--- vector.hpp
+++ vector.hpp
@@ -1,50 +1,36 @@
-#ifndef CONTAINERS_VECTOR_HPP
-#define CONTAINERS_VECTOR_HPP
+#ifndef VECTOR_HPP_
+#define VECTOR_HPP_
 
 #include <iostream>
+#include <iterator>
+#include <memory>
+#include <stdexcept>
+#include "./iterators/random_access_iterator.hpp"
+#include "./utils/utils.hpp"
+
 namespace ft {
-	template < class T, class Alloc = std::allocator<T> >
-	class vector {
-	private:
-		T	*arr;
-		size_t	size;
-		size_t	cap;
-		Alloc	alloc;
-	public:
-		typedef T														value_type;
-		typedef Alloc													allocator_type;
-		typedef size_t													size_type;
-		typedef typename allocator_type::reference						reference;
-		typedef typename allocator_type::const_reference				const_reference;
-		typedef typename allocator_type::pointer						pointer;
-		typedef typename allocator_type::const_pointer					const_pointer;
+template < class T, class Alloc = std::allocator<T> >
+class vector {
+ private:
+	T	*arr;
+	size_t	_size;
+	size_t	_cap;
+	Alloc	_alloc;
 
-//		vector(const allocator_type& alloc = allocator_type()) {
-//
-//		}
-//
-//		vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) {
-//
-//		}
-//
-//		template<class InputIterator>
-//				vector(InputIterator first, InputIterator last,  const allocator_type& alloc = allocator_type())
-//		{}
-//		vector(const vector& x) {
-//
-//		}
-//
-//		~vector() {
-//
-//		}
-//
-//		vector &operator=(const vector & x) {
-//			return <#initializer#>;
-//		}
-	};
-}
+ public:
+	typedef T										value_type;
+	typedef Alloc										allocator_type;
+	typedef size_t										size_type;
+	typedef ptrdiff_t									difference_type;
+	typedef value_type&									reference;
+	typedef const value_type&								const_reference;
+	typedef typename allocator_type::pointer						pointer;
+	typedef typename allocator_type::const_pointer						const_pointer;
+	typedef random_access_iterator< value_type, difference_type, pointer, reference>	iterator;
+	typedef random_access_iterator< value_type, difference_type, pointer, reference, true>	const_iterator;
+	// typedef reverse_iterator<iterator>									reverse_iterator;
+	// typedef reverse_iterator<const_iterator>							const_reverse_iterator;
 
-#endif //CONTAINERS_VECTOR_HPP
 //========================================================================================================
 // constructor/destructor
 //========================================================================================================
@@ -253,3 +239,8 @@ namespace ft {
 		}
 	}
 
+
+
+} // namespace ft
+
+#endif // VECTOR_HPP_
