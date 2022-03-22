c6c0a1ca65142a48c2e36366a010d4d5ae037e00
Talyx <farruh.rahmatov40@gmail.com>
2022-03-22 17:04:37 +0300

    add: meta func enable_if, is_integral

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -64,5 +64,81 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 // template <typename Iterator>
 // bool is_iterator( Iterator first, Iterator second) {
 
+//========================================================================================================
+// is_integral
+//========================================================================================================
+
+template <typename T>
+struct is_integral {
+	static const bool value = false;
+};
+
+template <>
+struct is_integral<bool> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<signed char> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<unsigned char> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<char> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<wchar_t> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<short> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<unsigned short> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<int> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<unsigned int> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<long> {
+	static const bool value = true;
+};
+
+template <>
+struct is_integral<unsigned long> {
+	static const bool value = true;
+};
+
+//========================================================================================================
+// enable_if
+//========================================================================================================
+
+template <bool, typename T = void>
+struct enable_if { };
+
+template <typename T>
+struct enable_if<true, T> {
+	typedef T type;
+};
+
 } // namespace ft
 #endif // UTILS_UTILS_HPP_
