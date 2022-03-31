ea6ce1c33be58ccec7fbbdb3330256c4c64656e5
Talyx <farruh.rahmatov40@gmail.com>
2022-03-31 21:24:33 +0300

    refactor: member types

--- map.hpp
+++ map.hpp
@@ -5,6 +5,7 @@
 #include <memory>
 #include "./utils/utils.hpp"
 #include "iterators/reverse_iterator.hpp"
+#include "./utils/BinTree.hpp"
 
 namespace ft {
 template < class Key, class T, class Compare = ft::less<Key>,
@@ -13,15 +14,24 @@ class map {
  public:
 	typedef Key					key_type;
 	typedef T					mapped_type;
-	typedef ft::pair<const Key, T>			value_type;
-	typedef size_t					size_type;
-	typedef ptrdiff_t				difference_type;
-	typedef Compare					key_compare;
-	typedef Allocator				allocator_type;
-	typedef value_type&				reference;
-	typedef const value_type&			const_reference;
-	typedef typename Allocator::pointer		pointer;
-	typedef typename Allocator::const_pointer	const_pointer;
+	typedef size_t				size_type;
+	typedef ptrdiff_t			difference_type;
+	typedef Compare				key_compare;
+	typedef Allocator			allocator_type;
+	typedef typename allocator_type::reference			reference;
+	typedef typename allocator_type::const_reference 	const_reference;
+	typedef typename allocator_type::pointer			pointer;
+	typedef typename allocator_type::const_pointer		const_pointer;
+	typedef ft::pair<const Key, T>						value_type;
+private:
+	typedef typename ft::Node<value_type>				node;
+	typedef typename Allocator::template rebind<typename  ft::Node<value_type> >::other allocator_node;
+	typedef typename ft::BinTree<value_type, key_compare, node, allocator_type, allocator_node> binTree;
+public:
+	typedef typename binTree::iterator iterator;
+	typedef typename binTree::const_iterator const_iterator;
+	typedef ft::reverse_iterator<iterator>		reverse_iterator;
+	typedef ft::reverse_iterator<const_iterator>	const_reverse_iterator;
 
 	class value_compare :
 	public ft::binary_function < value_type, value_type, bool> {
