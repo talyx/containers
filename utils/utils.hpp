edcc5f2d2d4108ae40a2f0ef028882a9e6089e53
Talyx <farruh.rahmatov40@gmail.com>
2022-04-04 18:24:49 +0300

    func equal

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -76,11 +76,12 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 template <class  InputIterator1, class InputIterator2>
 bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2) {
 	while (first1 != last1) {
-		if (first2 == last2 && first1 != first2)
+		if ((first2 == last2 )|| (first1 != first2))
 			return (false);
 		first1++;
 		first2++;
 	}
+	return (first2 == last2);
 }
 
 //========================================================================================================
