c42c3d8988e05d78c284a03a8217067faea08c3c
Talyx <farruh.rahmatov40@gmail.com>
2022-04-08 18:49:25 +0300

    fix: make_pair, equal

--- utils/utils.hpp
+++ utils/utils.hpp
@@ -76,8 +76,9 @@ bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
 template <class  InputIterator1, class InputIterator2>
 bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2) {
 	while (first1 != last1) {
-		if ((first2 == last2 )|| (first1 != first2))
+		if ((first2 == last2 )|| (*first1 != *first2)) {
 			return (false);
+		}
 		first1++;
 		first2++;
 	}
@@ -225,7 +226,7 @@ struct pair {
 //=========================================================
 
 template <class T1, class T2>
-	ft::pair<T1,T2> make_pair(const T1& x, const T2& y) {
+	ft::pair<T1,T2> make_pair(T1 x,  T2 y) {
 		return (ft::pair<T1, T2>(x, y));
 	}
 
